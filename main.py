153724dbc350923d7fb4b40e54ff2b6268c66df2
hardsuitt <129071970+MaksimSinyu@users.noreply.github.com>
2024-05-03 16:56:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import sqlite3
 import config
 import logging
 
+api_id = 24120751  # Замените на ваш API ID
+api_hash = 'acaa33628ae3a74cf956a6826e7779de'  # Замените на ваш API Hash
+
 # Настройка логгирования
-client = TelegramClient('new_session2', config.api_id, config.api_hash)
+client = TelegramClient('new_session2', api_id, api_hash)
 
 @client.on(events.NewMessage)
 async def handle_new_message(event):
