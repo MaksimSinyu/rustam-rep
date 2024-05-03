9096e6bd0511079a435aa1c005d150cc9cef8007
hardsuitt <129071970+MaksimSinyu@users.noreply.github.com>
2024-05-03 17:05:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from telethon import TelegramClient, events
-import sqlite3
-import config
-import logging
 
 api_id = 24120751  # Замените на ваш API ID
 api_hash = 'acaa33628ae3a74cf956a6826e7779de'  # Замените на ваш API Hash
+phone = '+380 93 209 68 65'  # Ваш номер телефона
 
-# Настройка логгирования
-client = TelegramClient('new_session2', api_id, api_hash)
+client = TelegramClient("ses", api_id, api_hash)
 
 @client.on(events.NewMessage)
 async def handle_new_message(event):
-    logging.info(f"Received message from {event.chat_id}: {event.text}")
+    print(f"Received message: {event.text}")
 
 async def main():
-    await client.start()  # Теперь требуется ввести номер телефона
-    print("Клиент запущен. Ожидание сообщений...")
+    await client.start()
+    print("Client started. Waiting for messages...")
     await client.run_until_disconnected()
 
 if __name__ == '__main__':
