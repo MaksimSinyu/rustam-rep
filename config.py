80d2b74cd712215da205e357db2e65e3204b7a33
hardsuitt <129071970+MaksimSinyu@users.noreply.github.com>
2024-05-03 18:37:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 api_id = '24120751'
 api_hash = 'acaa33628ae3a74cf956a6826e7779de'
-channel_usernames = ['-1001633826572']  # Пример списка каналов
+channel_usernames = ['@mediap2p_chat', '@BeribitRussia', '@p2p_lab_chat', '@pitupishka', '@HuobiCISp2p']
